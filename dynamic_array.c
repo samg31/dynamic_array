020e6efda206a8edaeb8d8e3abaa5b17fe1e85cb
samgoodrick <sdg31@zips.uakron.edu>
2017-04-10 14:59:22 -0400

    implemented resize/construct

---
+++ dynamic_array.c
@@ -0,0 +1,30 @@
+#include "dynamic_array.h"
+#include <malloc.h>
+
+void construct( dynamic_array* arr )
+{
+	arr->capacity = 4;
+	arr->array = malloc( arr->capacity * sizeof( double ) );
+
+	for( int i = 0; i < arr->capacity; ++i )
+		arr->array[i] = 0.0;
+}
+
+void resize( dynamic_array* arr )
+{
+	double* temp;
+	temp = realloc( arr->array, arr->capacity * 2 * sizeof( double ) );
+
+	if( temp == NULL )
+	{
+		printf( "unable to reallocate array %d\n", arr );
+		return;
+	}
+
+	for( int i = arr->capacity; i < arr->capacity * 2; ++i )
+	{
+		arr->array[i] = 0.0;
+	}
+
+	arr->capacity *= 2;
+}
